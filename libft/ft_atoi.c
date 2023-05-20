72c0219d1442d37b7c358a23fdeaa17d6ef210ff
cawauchi6204 <cawauchi6204@gmail.com>
2023-05-20 12:01:27 +0900

    add ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -10,6 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi() {
-	
+int	ft_atoi(char *str) {
+	int i;
+	int num;
+	int sign;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (num * sign);
+}
+
+#include <stdio.h>
+
+int main (void)
+{
+	int i = ft_atoi("-2147483690");
+	printf("%d", i);
+
+	return (0);
 }
