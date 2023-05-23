e123d0e11ffd29d777748fcc8723591068415399
Tatsuya Kawauchi <tkawauch@c1r19s5.42tokyo.jp>
2023-05-23 20:30:13 +0900

    add bzero

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,18 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkawauch <tkawauch@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/22 19:17:41 by tkawauch          #+#    #+#             */
-/*   Updated: 2023/05/23 20:30:03 by tkawauch         ###   ########.fr       */
+/*   Created: 2023/05/23 20:06:48 by tkawauch          #+#    #+#             */
+/*   Updated: 2023/05/23 20:29:51 by tkawauch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_tolower(int c)
+#include <stdio.h>
+
+void	ft_bzero(void *s, size_t n)
 {
-	if (c >= 'A' && c <= 'Z')
-		c += 32;
-	return (c);
+	unsigned char	*p;
+	int				i;
+
+	i = 0;
+	p = s;
+	while (n--)
+	{
+		p[i] = 0;
+		i++;
+	}
 }
+
+// #include <string.h>
+
+// int	main(void)
+// {
+// 	char	str1[] = "hogehoge";
+// 	char	str2[] = "fugafuga";
+// 	ft_bzero(str1, 3);
+// 	bzero(str2, 3);
+
+// 	printf("%s", str1);
+// 	printf("%s", str2);
+
+// 	return (0);
+// }
